74bb033a06290314128363eea5250c1cbfb11399
Alessandro_Mondelli <alexs98@hotmail.it>
2020-02-11 12:53:32 +0100

    Inizio header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Boolean Laravel</title>
+        <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+
+    <body>
+        <header>
+            <div class="container-fluid">
+                <nav id="menu" class="navbar navbar-expand-lg">
+                  <a id="logo" class="navbar-brand" href=""><img src="https://www.boolean.careers/images/common/logo.png" alt="logo"></a>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto text-uppercase">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Corso</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Dopo il Corso</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Lezione Gratuita</a>
+                        </li>
+                        <button class="btn btn-outline-success text-uppercase" type="button">Candidati Ora</button>
+                    </ul>
+                  </div>
+                </nav>
+            </div>
+        </header>
+    </body>
+
+</html>
