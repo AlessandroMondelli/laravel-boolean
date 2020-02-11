48594837a2d79c3105c706132ebc48658480373a
Alessandro_Mondelli <alexs98@hotmail.it>
2020-02-11 17:02:04 +0100

    Aggiunta grafica footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
         <title>Boolean Laravel</title>
         <link href="https://fonts.googleapis.com/css?family=Rubik:300,400&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="https://kit.fontawesome.com/fff0ef5f89.js" crossorigin="anonymous"></script>
     </head>
 
     <body>
@@ -80,6 +81,46 @@
                     </div>
                 </div>
             </section>
+            <footer>
+                <div id="bool-footer">
+                    <div class="container">
+                        <div class="row upper-footer">
+                            <div class="col-lg-6 sez-foot">
+                                <div id="footer-logo">
+                                    <img src="https://www.boolean.careers/images/common/logo-white.png" alt="logo boolean">
+                                </div>
+                                <p>Sede Operativa</p>
+                                <p>Via Carducci 12 - 20123 Milano</p>
+                                <p>Tel: 02-36599712</p>
+                            </div>
+                            <div class="col-lg-6 text-right sez-foot">
+                                <ul>
+                                    <li><a href="#">Lezione gratuita</a></li>
+                                    <li><a href="#">Domande frequenti</a></li>
+                                    <li><a href="#">Per le aziende</a></li>
+                                    <li><a href="#">Privacy policy</a></li>
+                                    <li><a href="#">Lavora con noi</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="row down-footer">
+                            <div class="col-lg-3">
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                            </div>
+                            <div class="col-lg-3">
+                                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            </div>
+                            <div class="col-lg-3">
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                            </div>
+                            <div class="col-lg-3">
+                                <a href="#"><i class="fab fa-youtube"></i></a>
+                            </div>
+                            <p>Boolean SRL - Piazzale Giovanni dalle Bande Nere, 9 - 20146, Milano - P.IVA: 10214930967</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </main>
     </body>
 
