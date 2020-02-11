337e0b8e8a3c8562144c1b4889d939f3cac7511a
Alessandro_Mondelli <alexs98@hotmail.it>
2020-02-11 15:58:21 +0100

    Aggiunto jumbo e seconda sezione

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Boolean Laravel</title>
-        <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Rubik:300,400&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
 
@@ -32,6 +32,55 @@
                 </nav>
             </div>
         </header>
+
+        <main>
+            <section>
+                <div id="jumbo">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="jumbotron">
+                                    <h1 class="text-uppercase">Diventa <strong>sviluppatore web</strong></h1>
+                                    <p class="lead font-weight-bold">Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</p>
+                                    <ul>
+                                        <li><strong>6 mesi</strong> di corso intensivo online in diretta</li>
+                                        <li><strong>Nessuna competenza</strong> di programmazione richiesta</li>
+                                        <li>Siamo certi del tuo successo, altrimenti <strong>ti rimborsiamo</strong></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="jumbotron">
+                                    <div id="bool-img">
+                                        <img src="https://www.boolean.careers/images/homepage/pc-black-gif.gif" alt="boolean img">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div id="percentuali">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <h2>98%</h2>
+                                <p>La percentuale dei nostri studenti che ora lavora come web developer, oltre la metà a tempo indeterminato.</p>
+                            </div>
+                            <div class="col-lg-4">
+                                <h2>€ 23.000</h2>
+                                <p>Lo stipendio medio lordo di partenza degli studenti assunti dalle nostre aziende partner.</p>
+                            </div>
+                            <div class="col-lg-4">
+                                <h2>#1</h2>
+                                <p>Siamo il primo istituto online in Italia per indice di gradimento e risultati conseguiti.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     </body>
 
 </html>
